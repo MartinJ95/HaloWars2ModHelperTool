3d65302407f278ab2704c829ff3475ee542f705d
MartinJ95 <martinjones95@gmx.co.uk>
2023-12-07 15:47:54 +0000

    feat attribute display : protected variables
    this commit should allow the attributes for display to be used by abstracted types.

--- HaloWars2ModCreationTool/HaloWars2ModCreationTool/AttributeDisplay.h
+++ HaloWars2ModCreationTool/HaloWars2ModCreationTool/AttributeDisplay.h
@@ -6,7 +6,15 @@ class AttributeDisplay
 public:
 	virtual void Display() const = 0;
 	virtual void EditValues() = 0;
-private:
+	unsigned int GetID() const
+	{
+		return ID;
+	}
+	const std::string& GetName() const
+	{
+		return name;
+	}
+protected:
 	unsigned int ID;
 	std::string name;
 
