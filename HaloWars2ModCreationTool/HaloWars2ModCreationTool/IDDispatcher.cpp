432eaa70dc3b594d10745a0bfac9e3c2c2f37fbd
MartinJ95 <martinjones95@gmx.co.uk>
2023-12-08 00:24:23 +0000

    feat id dispatcher : id dispatcher
    this commit should provide the initial implemtation of a framework to allow for unique id's to be claimed and released

---
+++ HaloWars2ModCreationTool/HaloWars2ModCreationTool/IDDispatcher.cpp
@@ -0,0 +1,61 @@
+#include "IDDispatcher.h"
+
+IDDispatcher::IDDispatcher() : m_current(0), m_size(10), m_data(new unsigned int[10])
+{
+}
+
+unsigned int IDDispatcher::ClaimID()
+{
+	m_current++;
+	if (m_size == m_current)
+	{
+		Resize(m_size + 1);
+	}
+	return m_data[m_current-1];
+}
+
+void IDDispatcher::ReleaseID(const unsigned int id)
+{
+	for (int i = 0; i < m_current; i++)
+	{
+		if (m_data[i] == id)
+		{
+			m_current--;
+			if (m_current > 0)
+			{
+				Swap(i, m_current);
+			}
+		}
+	}
+}
+
+
+void IDDispatcher::Resize(const unsigned int newSize)
+{
+	unsigned int oldSize = m_size;
+	unsigned int* oldData = m_data;
+
+	m_data = new unsigned int[newSize];
+	for (unsigned int i = 0; i < oldSize && i < newSize; i++)
+	{
+		m_data[i] = oldData[i];
+	}
+	for (unsigned int i = oldSize; i < newSize; i++)
+	{
+		m_data[i] = i;
+	}
+
+	delete[] oldData;
+}
+
+void IDDispatcher::Swap(const unsigned int first, const unsigned int second)
+{
+	unsigned int temp = m_data[first];
+	m_data[first] = m_data[second];
+	m_data[second] = temp;
+}
+
+IDDispatcher::~IDDispatcher()
+{
+	delete[] m_data;
+}
---
+++ HaloWars2ModCreationTool/HaloWars2ModCreationTool/IDDispatcher.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+class IDDispatcher
+{
+public:
+	IDDispatcher();
+	unsigned int ClaimID();
+	void ReleaseID(const unsigned int id);
+	~IDDispatcher();
+private:
+	void Resize(const unsigned int newSize);
+	void Swap(const unsigned int first, const unsigned int second);
+	unsigned int m_current;
+	unsigned int m_size;
+	unsigned int* m_data;
+};
+
