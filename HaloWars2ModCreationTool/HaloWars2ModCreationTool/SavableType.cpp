87e04e75fe16b91a7c10960c6724990306db0737
MartinJ95 <martinjones95@gmx.co.uk>
2023-12-14 01:29:08 +0000

    feat savabletype : savable type container
    this commit should allow for a new savable type derived class that can managed the saving and loading of containers

--- HaloWars2ModCreationTool/HaloWars2ModCreationTool/SavableType.cpp
+++ HaloWars2ModCreationTool/HaloWars2ModCreationTool/SavableType.cpp
@@ -1,1 +1,6 @@
 #include "SavableType.h"
+
+uint8_t SavableTypeContainer::GetFlags()
+{
+    return m_flags;
+}
--- HaloWars2ModCreationTool/HaloWars2ModCreationTool/SavableType.h
+++ HaloWars2ModCreationTool/HaloWars2ModCreationTool/SavableType.h
@@ -8,3 +8,17 @@ public:
 	virtual void Load(std::ifstream &stream) = 0;
 };
 
+enum class SavableTypeContainerFlags
+{
+	eDefault = 0b00000000,
+	eSave = 0b00000000,
+	eLoad = 0b00000000
+};
+
+class SavableTypeContainer : public SavableType
+{
+public:
+	uint8_t GetFlags();
+protected:
+	uint8_t m_flags = (uint8_t)SavableTypeContainerFlags::eDefault;
+};
